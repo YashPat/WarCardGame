468192f76456979cbded8634604cc0fc7ca8c412
YashPat <pyash@outlook.com>
2018-10-20 17:00:41 -0500

    Add files via upload

---
+++ War/War/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  War
+//
+//  Created by Patil, Yash M on 10/5/18.
+//  Copyright © 2018 Patil, Yash M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func dealTapped(_ sender: Any)
+    {
+        let leftRandomNumber = arc4random_uniform(13)+2
+        let rightRandomNumber = arc4random_uniform(13)+2
+        leftImageView.image = UIImage(named:"card\(leftRandomNumber)")
+        rightImageView.image = UIImage(named:"card\(rightRandomNumber)")
+        if leftRandomNumber > rightRandomNumber
+        {
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        }
+        if rightRandomNumber > leftRandomNumber
+        {
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
+        else if leftRandomNumber == rightRandomNumber
+        {
+        }
+    }
+}
+
